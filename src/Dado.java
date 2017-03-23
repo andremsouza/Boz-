5cc709a50677bec436b14ef36eba84b8b5f43ce1
andremsouza <msouza.andre@hotmail.com>
2017-03-23 09:48:44 -0300

    Finished class Dado

--- src/Dado.java
+++ src/Dado.java
@@ -1,18 +1,29 @@
 
 public class Dado {
+	//parametros para geração
+	private int n = -1;
+	private int l = 6;
+	private Random r = new Random();
 	//Constructors
 	public Dado(){
-		
+		n = rolar();
+		//n = r.getIntRand(l+1);
+		//while(n<=0)	n = r.getIntRand(l+1);
 	}
 	public Dado(int n){
-		
+		l = n;
+		n = rolar();
+		//n = r.getIntRand(l+1);
+		//while(n<=0)	n = r.getIntRand(l+1);
 	}
 	//Methods
 	public int getLado(){
-		
+		return n;
 	}
 	public int rolar(){
-		
+		n = r.getIntRand(l+1);
+		while(n<=0)	n = r.getIntRand(l+1);
+		return n;
 	}
 	public String toString(){
 		String s;
@@ -40,6 +51,7 @@ public class Dado {
 		//|  *  |\n
 		//|*   *|\n
 		s.concat("+-----+\n");
+		return s;
 	}
 	
 	
